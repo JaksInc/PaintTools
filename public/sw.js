bbcab3a357e881dedeebd8c82b753f5d10186d14
Justin <commaster777@gmail.com>
2021-11-08 20:53:55 -0500

    Fix results area to be nicely formatted Fixes #5 Results now show up in table

--- public/sw.js
+++ public/sw.js
@@ -13,17 +13,17 @@ const assets = [
 ]
 
 self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-      caches.open("sw-cache").then(cache => {
-        cache.addAll(assets)
-      })
-    )
-  })
+  installEvent.waitUntil(
+    caches.open("sw-cache").then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
 
-  self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
